8907f1bb7b37e8828cb99e0a9c6a1728c8b1f10d
Peter May <Peter.May@bl.uk>
2018-05-12 13:09:19 +0100

    Added a 'metric' argument to the compare_tiffs module to allow for the possibility of other compare metrics, e.g. PSNR

--- tifinity/modules/compare_tiffs.py
+++ tifinity/modules/compare_tiffs.py
@@ -4,15 +4,19 @@ from tifinity.parser.tiff import Tiff
 
 
 class CompareTiffs(BaseModule):
-    """ Module comparing the similarity of two TIFF files.
-        By default this compares the MD5 checksums of the pixel image data. """
+    """ Module comparing the similarity of two TIFF files according to some metric.
+        Currently only comparison via the MD5 checksums of the file & pixel data is supported."""
 
     def __init__(self):
         self.cli_name = 'compare'
+        self.metric_choices = ["checksum"]      # other metric choices may be added
 
     def add_subparser(self, mainparser):
         m_parser = mainparser.add_parser(self.cli_name)
         m_parser.set_defaults(func=self.process_cli)
+
+        m_parser.add_argument("-m", "--metric", dest="metric", choices=self.metric_choices, required=True)
+
         m_parser.add_argument("tiff1", help="the original TIFF file to compare")
         m_parser.add_argument("tiff2", help="the comparison TIFF file")
 
@@ -28,26 +32,27 @@ class CompareTiffs(BaseModule):
             raise
 
         # calculate checksums
-        checksums = [Checksum.checksum(t) for t in tiffs]
+        if args.metric=="checksum":
+            checksums = [Checksum.checksum(t) for t in tiffs]
 
-        self.returnValues = {}
+            self.returnValues = {}
 
-        # identical files?
-        if checksums[0]["full"]==checksums[1]["full"]:
-            self.returnValues["Files Identical"] = True
-            print ("Files Identical")
+            # identical files?
+            if checksums[0]["full"]==checksums[1]["full"]:
+                self.returnValues["Files Identical"] = True
+                print ("Files Identical")
+                return self.returnValues
+
+            self.returnValues["Files Identical"] = False
+            image_identical = {}
+            i = 0
+            for i_orig in checksums[0]["images"]:
+                image_identical[i] = []                             # for each image in the original TIFF
+                for i_other in checksums[1]["images"]:
+                    image_identical[i].append(i_orig == i_other)    # compare against each image in the comparison TIFF
+                i += 1
+            self.returnValues["Images Identical"] = image_identical
+
             return self.returnValues
-
-        self.returnValues["Files Identical"] = False
-        image_identical = {}
-        i = 0
-        for i_orig in checksums[0]["images"]:
-            image_identical[i] = []                             # for each image in the original TIFF
-            for i_other in checksums[1]["images"]:
-                image_identical[i].append(i_orig == i_other)    # compare against each image in the comparison TIFF
-            i += 1
-        self.returnValues["Images Identical"] = image_identical
-
-        return self.returnValues
 
 module = CompareTiffs()     # initiate module class when module imported
