77cbc2fe3aed666c4a727014eb35a40e29346947
Peter May <Peter.May@bl.uk>
2018-05-12 13:11:35 +0100

    Updated unit test module

--- tests/test_module_compare_tiffs.py
+++ tests/test_module_compare_tiffs.py
@@ -16,20 +16,20 @@ class TestModuleCompareTiffs(unittest.TestCase):
         res_path = "t_one_strip"
         path = os.path.join("./resources", res_path)
         file = os.path.join(path, res_path + ".tiff")
-        args = Namespace(files=[file])
-        output = compare_tiffs.module.process_cli(args)
+        args = Namespace(tiff1=file)
 
-        self.assertEqual(compare_tiffs.module.ModuleReturnCodes["NotEnoughFiles"], output)
+        with self.assertRaises(AttributeError):
+            compare_tiffs.module.process_cli(args)
 
     def test_identical_file(self):
-        """ Tests that pixel checksums are valid if two identical files are supplied """
+        """ Tests that pixel checksums are identical if two identical files are supplied """
         res_path = "t_one_strip"
         path = os.path.join("./resources", res_path)
         file = os.path.join(path, res_path + ".tiff")
-        args = Namespace(files=[file, file])
+        args = Namespace(tiff1=file, tiff2=file, metric="checksum")
         output = compare_tiffs.module.process_cli(args)
 
-        self.assertEqual({"Files Identical": True}, output)
+        self.assertEqual({'Files Identical': True}, output)
 
     def test_different_files(self):
         """ Tests that different TIFFs return correct output """
@@ -41,11 +41,11 @@ class TestModuleCompareTiffs(unittest.TestCase):
         comp_path = os.path.join("./resources", comp_res_path)
         comp_file = os.path.join(comp_path, comp_res_path + ".tiff")
 
-        args = Namespace(files=[orig_file, comp_file])
+        args = Namespace(tiff1=orig_file, tiff2=comp_file, metric="checksum")
         output = compare_tiffs.module.process_cli(args)
 
-        self.assertEqual({"Files Identical": False,
-                          "Images Identical": {0: [False]}})
+        self.assertEqual({'Files Identical': False,
+                          'Images Identical': {0: [False]}}, output)
 
 if __name__ == '__main__':
     unittest.main()
